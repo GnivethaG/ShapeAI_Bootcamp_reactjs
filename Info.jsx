f668ef51de9a2dbc4b839039ccc899315708517d
GnivethaG <86570421+GnivethaG@users.noreply.github.com>
2021-06-28 00:08:07 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info(){
+  return(
+  <div className="note">
+  <h1> Javascript and React.js</h1>
+  <p>A basic web dev boot React js camp</p>
+  </div>
+  );
+  }
+  export default Info;
